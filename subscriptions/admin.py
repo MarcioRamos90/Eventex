9e94d8bf8ad7e59603f64c44c1266fb7bbb95b2c
Márcio Ramos <marcioalvesramos90@hotmail.com>
2021-04-21 22:31:06 -0300

    admin magic

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,3 +1,18 @@
+from django.utils.timezone import now
 from django.contrib import admin
+from subscriptions.models import Subscription
 
-# Register your models here.
+class SubscriptionModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'created_at', 'subscribe_today')
+    date_hierarchy = 'created_at'
+    search_fields = ('name', 'email', 'phone', 'created_at')
+    list_filter = ('created_at',)
+
+    def subscribe_today(self, obj):
+        return obj.created_at == now().date()
+
+    subscribe_today.short_description = 'inscrito hoje?'
+    subscribe_today.boolean = True
+
+
+admin.site.register(Subscription, SubscriptionModelAdmin)
